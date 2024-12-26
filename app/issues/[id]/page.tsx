731a91f72e1266b378bf16b4e9b9ae3a7279a3a8
jiali <mahyarerad@gmail.com>
2024-12-27 01:54:16 +0330

    Refactor: Apply the SRP

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,10 +1,8 @@
-import { IssueStatusBadge } from "@/app/components";
 import prisma from "@/prisma/client";
-import { Box, Button, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes";
-import Link from "next/link";
+import { Box, Flex, Grid } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
-import ReactMarkdown from "react-markdown";
-import { Pencil2Icon } from "@radix-ui/react-icons";
+import IssueDetails from "./IssueDetails";
+import IssueEditButton from "./IssueEditButton";
 
 interface Props {
   params: { id: string };
@@ -19,20 +17,10 @@ const IssueDetailsPage = async ({ params }: Props) => {
   return (
     <Grid columns={{ initial: "1", md: "2" }} gap="5">
       <Flex direction="column" gap="2">
-        <Heading as="h2">{issue.title}</Heading>
-        <Flex gap="3" my="2">
-          <IssueStatusBadge status={issue.status} />
-          <Text>{issue.createdAt.toDateString()}</Text>
-        </Flex>
-        <Card className="prose">
-          <ReactMarkdown>{issue.description}</ReactMarkdown>
-        </Card>
+        <IssueDetails issue={issue} />
       </Flex>
       <Box>
-        <Button>
-          <Pencil2Icon />
-          <Link href={`/issue/${issue.id}/edit`}>Edit Issue</Link>
-        </Button>
+        <IssueEditButton issueId={issue.id} / >
       </Box>
     </Grid>
   );
